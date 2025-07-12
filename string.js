f9fcf90afc47e4ba196dc8dd4c94868cba079de2
Nayan <n007kingkong@gmail.com>
2025-07-12 22:59:15 +0600

    some rules of declaring string with some examples

---
+++ string.js
@@ -0,0 +1,41 @@
+
+    //  Differentt types of declaring string { "" , ''  , ``}
+
+var para1 = "Hello everyone am new in javaScript"
+
+
+var para2 = 'Hello everyone am new in javaScript.I am dedicated to learning javaScript.'
+
+
+var para3 = `Hello everyone am new in javaScript.I am dedicated in learning javaScript.I will work hard to become a programmer.`
+
+
+            //  lowercase conversion
+
+  
+ var lowercase = para1.toLowerCase();
+ console.log(lowercase);      
+
+
+   //  Upperercase conversion
+
+
+ var uppercase = para2.toUpperCase();
+ console.log(uppercase);  
+
+
+
+ console.log( para1.substring(6,20));
+
+
+ console.log( para1.indexOf('new'));
+
+
+ console.log( para1.split(' '));
+
+ console.log(para1.replace('new', 'beginner'));
+
+ 
+
+
+
