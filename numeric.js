5778dd7e1f995c1fe59f01c868f7e0d244922e3d
Nayan <n007kingkong@gmail.com>
2025-07-12 23:33:31 +0600

    some numeric functions

---
+++ numeric.js
@@ -0,0 +1,18 @@
+var num1 = 15.5;
+var num2 = '30.5';
+
+var num2 = parseFloat(num2);
+
+var num2 = +num2;
+
+var total = num1 + num2 ;
+console.log(total);
+
+
+
+var value1 = 0.1;
+var value2 = 0.2;
+
+var sum = value1 + value2;
+sum = sum.toFixed(1);
+console.log(sum);
