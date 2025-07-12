40d7a21cb4356408bb21b6897e693cc429047f34
Nayan <n007kingkong@gmail.com>
2025-07-13 00:03:53 +0600

    addition & concat

---
+++ addition.js
@@ -0,0 +1,16 @@
+
+
+var number1 = 20 ;
+var number2 =30;
+
+var sum = number1 + number2 ;
+console.log(sum);
+
+
+var firstName = 'Nayan';
+var lastName = 'Mondal';
+
+var concat = firstName + lastName;
+var concat = firstName + ' ' + lastName;
+console.log(concat);
+
