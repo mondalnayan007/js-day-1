d95e05b0fe20c9e6005fdd83c3f118d13e993234
Nayan <n007kingkong@gmail.com>
2025-07-13 00:47:08 +0600

    math. function

---
+++ math-function.js
@@ -0,0 +1,22 @@
+
+
+ var num1 = 12.56;
+ var num2 = 15.45;
+
+//  var value = 25.5478;
+
+// value = Math.round(value);
+// console.log(value);
+
+
+ var result = num1 + num2;
+// result = Math.ceil(result);
+// result = Math.floor(result);
+
+// console.log(result);
+
+ var lottery = Math.ceil(Math.random()*100);
+
+  console.log(lottery);
+
+ 
