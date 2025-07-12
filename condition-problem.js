823d501af17e4a5d91198d409483b2875d58b613
Nayan <n007kingkong@gmail.com>
2025-07-13 01:26:29 +0600

    Number grading system using else if condition

---
+++ condition-problem.js
@@ -0,0 +1,32 @@
+
+var mark = Math.floor(Math.random()*100);
+
+
+
+if(mark >= 80)
+{
+    console.log('You got A+');
+}
+else if(mark >= 70)
+{
+    console.log('You got A');
+}
+else if(mark >= 60)
+{
+    console.log('You got A-');
+}
+else if(mark >= 50)
+{
+    console.log('You got B');
+}
+else if(mark >= 40)
+{
+    console.log('You got C');
+}
+else if(mark >= 33)
+{
+    console.log('You got D');
+}
+else{
+    console.log('You have failed in this subject,Try again...');
+}
