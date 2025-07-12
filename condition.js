f71c22ad65355a9bdaf01b01fc565fcef21cb5f3
Nayan <n007kingkong@gmail.com>
2025-07-13 01:06:27 +0600

    if condition

---
+++ condition.js
@@ -0,0 +1,11 @@
+
+
+var value = Math.round(Math.random()*50);
+
+if(value > 25)
+{
+    console.log('Your value is greater than 25');
+}
+else{
+    console.log('Your value is smaller than 25');
+}
