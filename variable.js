d8097a2df9d30b18f3d113ce34fe8a873a1acf3e
Nayan <n007kingkong@gmail.com>
2025-07-12 22:17:15 +0600

    variables of js

---
+++ variable.js
@@ -0,0 +1,35 @@
+            //    All types of variables in js
+var price = 10;
+
+console.log(price);
+
+console.log(typeof price);
+
+
+var name = "Nayan"
+console.log(name);
+
+console.log(typeof name);
+
+
+
+var userName = "Shekh Mujibur Rahman";
+console.log(userName);
+console.log(typeof userName);
+
+
+var ans = true;
+var isRain = false;
+var willGo = true;
+
+console.log( ans);
+console.log(typeof ans);
+
+
+console.log(willGo);
+console.log(typeof willGo);
+
+
+console.log(isRain);
+console.log(typeof isRain);
+
